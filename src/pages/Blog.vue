ac3a280571a6eca86b37f0e9329456f4bf66f6d9
markmead <itsmarkmead@gmail.com>
2020-06-14 00:47:24 +0100

    Clean up deploy errors

--- src/pages/Blog.vue
+++ src/pages/Blog.vue
@@ -3,7 +3,7 @@
     <div class="max-w-screen-xl px-4 pt-12 mx-auto sm:pt-16 md:pt-20 sm:px-6">
       <h1 class="text-6xl leading-none text-center font-display">Blog</h1>
     </div>
-    <ul class="mt-8 -space-y-px">
+    <!-- <ul class="mt-8 -space-y-px">
       <app-blog-link
         v-for="blog in $page.blogs.edges"
         :key="blog.node.id"
@@ -12,32 +12,32 @@
         :title="blog.node.title"
         :teaser="blog.node.teaser"
       />
-    </ul>
+    </ul>-->
   </Layout>
 </template>
 
-<page-query>
-  query {
-    blogs: allBlogs(sortBy: "createdAt") {
-      edges {
-        node {
-          id
-          title
-          teaser
-          createdAt
-          path
-        }
-      }
-    }
-  }
-</page-query>
+// <page-query>
+//   query {
+//     blogs: allBlogs(sortBy: "createdAt") {
+//       edges {
+//         node {
+//           id
+//           title
+//           teaser
+//           createdAt
+//           path
+//         }
+//       }
+//     }
+//   }
+// </page-query>
 
 <script>
 import BlogLink from '~/components/BlogLink'
 
 export default {
   components: {
-    'app-blog-link': BlogLink,
-  },
+    'app-blog-link': BlogLink
+  }
 }
 </script>
