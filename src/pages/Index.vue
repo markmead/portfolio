161129bf3c5aa19fb272e91274b34df448b02624
markmead <itsmarkmead@gmail.com>
2020-06-16 23:08:48 +0100

    Add example page for negative-hover package

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -2,20 +2,16 @@
   <Layout>
     <div class="max-w-screen-xl px-4 pt-20 mx-auto sm:px-6">
       <h1 class="text-4xl leading-none md:text-6xl font-display">Mark Mead</h1>
-      <h2
-        class="text-3xl leading-tight opacity-75 md:text-5xl font-display"
-      >a web developer powered by Rails &amp; JavaScript</h2>
+      <h2 class="text-3xl leading-tight opacity-75 md:text-5xl font-display">
+        a web developer powered by Rails &amp; JavaScript
+      </h2>
       <div class="grid grid-cols-12 gap-8 mt-8">
         <div class="col-span-6 lg:col-span-4">
           <figure>
-            <g-image
-              src="~/assets/images/me-at-work.jpg"
-              class="w-full"
-              alt="Working in the office"
-            />
-            <figcaption
-              class="mt-1 text-xs font-bold text-center opacity-75 md:text-sm"
-            >Myself (left) and Jordi (right) pairing on a project</figcaption>
+            <g-image src="~/assets/images/me-at-work.jpg" class="w-full" alt="Working in the office" />
+            <figcaption class="mt-1 text-xs font-bold text-center opacity-75 md:text-sm">
+              Myself (left) and Jordi (right) pairing on a project
+            </figcaption>
           </figure>
         </div>
         <div class="col-span-6 lg:col-span-8">
@@ -26,6 +22,7 @@
             <app-nav-link-in-body path="/about" text="About" />
             <app-nav-link-in-body path="/resources" text="Resources" />
             <app-nav-link-in-body path="/site-info" text="Site info" />
+            <app-nav-link-in-body path="/negative-hover" text="Negative hover" />
           </nav>
         </div>
       </div>
@@ -45,17 +42,15 @@ export default {
         key: 'description',
         name: 'description',
         content:
-          "Hello, I'm Mark Mead. I'm a 23 year old web developer who has been building websites for 4 years. Working with Ruby on Rails, Javascript, Vue, React and more."
-      }
-    ]
+          "Hello, I'm Mark Mead. I'm a 23 year old web developer who has been building websites for 4 years. Working with Ruby on Rails, Javascript, Vue, React and more.",
+      },
+    ],
   },
   components: {
-    'app-nav-link-in-body': NavLinkInBody
+    'app-nav-link-in-body': NavLinkInBody,
   },
   beforeMount() {
-    axios
-      .get('https://dev.to/api/articles?username=markmead')
-      .then(res => console.log(res))
-  }
+    axios.get('https://dev.to/api/articles?username=markmead').then((res) => console.log(res))
+  },
 }
 </script>
