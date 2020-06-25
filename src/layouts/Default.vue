5f8732aad1bbeb83a5f9bacdffa64e4212fa64d0
markmead <itsmarkmead@gmail.com>
2020-06-25 20:37:12 +0100

    Add small fade in transition on pages

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -1,9 +1,10 @@
 <template lang="pug">
 div
   Navbar
-  main#main
-    div(class="max-w-3xl px-4 py-8 mx-auto sm:px-6 sm:py-12 md:py-16")
-      slot
+  transition(name="fade" appear)
+    main#main
+      div(class="max-w-3xl px-4 py-8 mx-auto sm:px-6 sm:py-12 md:py-16")
+        slot
 </template>
 
 <script>
@@ -11,7 +12,18 @@ import Navbar from '@/components/Navbar'
 
 export default {
   components: {
-    Navbar,
-  },
+    Navbar
+  }
 }
 </script>
+
+<style>
+.fade-enter-active {
+  transition: 0.2s ease-in-out;
+}
+
+.fade-enter {
+  opacity: 0;
+  transform: translateY(10px);
+}
+</style>
