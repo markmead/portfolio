a0f7f945c2c9f9e1e527172458a1e99d4a7a58fb
markmead <itsmarkmead@gmail.com>
2020-08-11 19:54:48 +0100

    Test PWA plugin

--- gridsome.config.js
+++ gridsome.config.js
@@ -4,7 +4,7 @@ const postcssPlugins = [tailwind()]
 module.exports = {
   siteName: 'Mark Mead',
   siteUrl: 'https://mead.im',
-  icon: './src/favicon.svg',
+  icon: './src/favicon.png',
   plugins: [
     { use: '@gridsome/plugin-sitemap' },
     {
@@ -13,6 +13,17 @@ module.exports = {
         typeName: 'Project',
         path: './src/content/project/*.md',
         route: '/work/:path',
+      },
+    },
+    {
+      use: 'gridsome-plugin-manifest',
+      options: {
+        background_color: '#FFF',
+        icon_path: './src/favicon.png',
+        name: 'Mark Mead Web Developer Portfolio',
+        short_name: 'Mark Mead Portfolio',
+        theme_color: '#000',
+        lang: 'en',
       },
     },
   ],
