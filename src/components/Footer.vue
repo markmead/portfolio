2e8543b00e372835221e76d6260b3c59c71c037b
markmead <itsmarkmead@gmail.com>
2020-11-16 17:25:43 +0000

    clean up footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,36 +2,30 @@
   <footer class="px-4 py-3 bg-white border-t border-gray-200">
     <p class="font-medium text-black text-opacity-50">
       Built with
-      <a
-        href="https://vuejs.org/"
-        target="_blank"
-        rel="nofollow"
-        class="underline hover:no-underline"
-      >Vue</a>,
-      <a
-        href="https://github.com/axios/axios"
-        target="_blank"
-        rel="nofollow"
-        class="underline hover:no-underline"
-      >Axios</a>,
-      <a
-        href="https://dev.to/"
-        target="_blank"
-        rel="nofollow"
-        class="underline hover:no-underline"
-      >DEV.to</a>,
-      <a
-        href="https://tailwindcss.com/"
-        target="_blank"
-        rel="nofollow"
-        class="underline hover:no-underline"
-      >Tailwind</a> and
-      <a
-        href="https://gridsome.org/"
-        target="_blank"
-        rel="nofollow"
-        class="underline hover:no-underline"
-      >Gridsome</a>.
+      <span v-for="(link, index) in links" :key="link.name">
+        <span v-if="index !== links.length - 1">
+          <a :href="link.path" target="_blank" rel="noreferrer" class="hover:underline" v-text="link.name"></a>,
+        </span>
+        <span v-if="index === links.length - 1">
+          and <a :href="link.path" target="_blank" rel="noreferrer" class="hover:underline" v-text="link.name"></a>
+        </span>
+      </span>
     </p>
   </footer>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      links: [
+        { name: 'Vue', path: 'https://vuejs.org/' },
+        { name: 'Axios', path: 'https://github.com/axios/axios' },
+        { name: 'DEV.to', path: 'https://dev.to/' },
+        { name: 'Tailwind', path: 'https://tailwindcss.com/' },
+        { name: 'Gridsome', path: 'https://gridsome.org/' },
+      ],
+    }
+  },
+}
+</script>
