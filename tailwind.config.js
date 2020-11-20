8a0f72501618242092441503414e11b8efc5c616
markmead <itsmarkmead@gmail.com>
2020-11-20 08:59:54 +0000

    apply a `markdown` class

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,19 +9,6 @@ module.exports = {
   },
   darkMode: false,
   theme: {
-    typography: (theme) => ({
-      default: {
-        css: {
-          color: theme('colors.gray.800'),
-          a: {
-            textDecoration: 'none',
-          },
-          strong: {
-            fontWeight: theme('fontWeight.medium'),
-          },
-        },
-      },
-    }),
     extend: {
       fontFamily: {
         mono: ['Roboto Mono', ...defaultTheme.fontFamily.mono],
