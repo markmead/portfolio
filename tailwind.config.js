64a0274db69d3517158507844e99363398d727f1
markmead <itsmarkmead@gmail.com>
2020-11-20 08:50:45 +0000

    pass safelist

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,12 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
-  purge: ['./src/**/*.vue'],
+  purge: {
+    content: ['./src/**/*.vue', './src/content/**/*.md'],
+    options: {
+      safelist: ['prose', 'prose-lg', 'h2', 'h3', 'h4', 'p', 'a', 'ul', 'li', 'strong'],
+    },
+  },
   darkMode: false,
   theme: {
     typography: (theme) => ({
