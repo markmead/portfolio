7da3db094a5413fa91b411b46d68f07e9fd7cb7b
Mark Mead <50486078+markmead@users.noreply.github.com>
2021-05-03 12:30:37 +0100

    Update main.js

--- src/main.js
+++ src/main.js
@@ -3,11 +3,6 @@ import '@fontsource/inter'
 
 import DefaultLayout from '@/layouts/Default'
 
-export default function(Vue, { head }) {
+export default function(Vue) {
   Vue.component('Layout', DefaultLayout)
-
-  head.script.push({
-    src: 'https://gumroad.com/js/gumroad.js',
-    body: true,
-  })
 }
