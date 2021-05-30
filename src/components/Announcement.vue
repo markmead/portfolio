1a37430c524379d3b3570f6b7ce3efb87e8e718c
Mark Mead <50486078+markmead@users.noreply.github.com>
2021-05-30 02:33:04 +0100

    Update Announcement.vue

--- src/components/Announcement.vue
+++ src/components/Announcement.vue
@@ -2,7 +2,7 @@
   <section class="flex items-center justify-center p-3 bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500">
     <p class="text-sm font-medium text-center text-white">
       HyperUI: Components for Tailwind CSS!
-      <a href="https://gum.co/uUMQe" class="underline">90+ Components for $20</a>
+      <a href="https://gum.co/uUMQe" class="underline">100+ Components for $10</a>
     </p>
   </section>
 </template>
