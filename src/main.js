e87c3026076fc7128e7fb5810c0635b60bde0973
markmead <itsmarkmead@gmail.com>
2021-06-03 11:53:49 +0100

    update font weights

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import 'tailwindcss/tailwind.css'
-import '@fontsource/inter'
+import '@fontsource/inter/400.css'
+import '@fontsource/inter/500.css'
+import '@fontsource/inter/700.css'
 
 import DefaultLayout from '@/layouts/Default'
 
