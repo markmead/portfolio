ffc6519a4f1f2a32abb290a6998f0bfa2cbc9083
markmead <itsmarkmead@gmail.com>
2021-06-03 12:17:14 +0100

    make announcement dynamic

--- src/components/Announcement.vue
+++ src/components/Announcement.vue
@@ -2,7 +2,22 @@
   <section class="flex items-center justify-center p-3 bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500">
     <p class="text-sm font-medium text-center text-white">
       HyperUI: Components for Tailwind CSS!
-      <a href="https://gum.co/uUMQe" class="underline">100+ Components for $10</a>
+      <a href="https://gum.co/uUMQe" class="underline">{{ count }} Components for $10</a>
     </p>
   </section>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      count: '100+',
+    }
+  },
+  mounted() {
+    fetch('https://www.hyperui.dev/data.json', { method: 'get' })
+      .then((response) => response.json())
+      .then((data) => (this.count = data.count))
+  },
+}
+</script>
