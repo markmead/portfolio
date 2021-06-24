64d0b9ff883cffa9148baac05b1bbee258efff4f
markmead <itsmarkmead@gmail.com>
2021-06-24 09:11:51 +0100

    update announcement

--- src/components/Announcement.vue
+++ src/components/Announcement.vue
@@ -1,16 +1,18 @@
 <template>
-  <section class="flex items-center justify-center p-3 bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500">
-    <p class="text-sm font-medium text-center text-white">
-      HyperUI: Components for Tailwind CSS!
-      <a
-        href="https://gum.co/uUMQe"
-        class="underline"
-        target="_blank"
-        rel="noreferrer"
-      >
-        {{ count }} Components for {{ price }}
-      </a>
-    </p>
+  <section class="text-center bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500">
+    <div class="container py-3">
+      <p class="text-sm font-medium text-white">
+        HyperUI: Components for Tailwind CSS!
+        <a
+          href="https://www.hyperui.dev/"
+          class="underline"
+          rel="noreferrer"
+          target="_blank"
+        >
+          {{ count }} Components for {{ price }}
+        </a>
+      </p>
+    </div>
   </section>
 </template>
 
@@ -18,16 +20,18 @@
 export default {
   data() {
     return {
-      count: '150+',
-      price: '$15',
+      count: '',
+      price: '',
     }
   },
   mounted() {
     fetch('https://www.hyperui.dev/data.json', { method: 'get' })
       .then((response) => response.json())
       .then((data) => {
+        const price = data.price.substring(1)
+
         this.count = data.count
-        this.price = data.price
+        this.price = `$${price}`
       })
   },
 }
